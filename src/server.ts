d3289a7c17f58283e26435c655ae72d75101b846
vamsi krishna <kvamsi.nellore@gmail.com>
2026-05-21 15:54:22 +0530

    Add task-service

--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from './app.js';
-import './config/db.js';
-import './models/userModel.js';
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is listening");
-});
